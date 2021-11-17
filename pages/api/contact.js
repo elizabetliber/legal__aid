bcb16627778c4c3cb5ac563b4c4e25724a38f0f4
lisa <elizabet.libet@gmail.com>
2021-11-17 23:46:34 +0500

    edit form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,17 @@
-import sendEmail from './../../lib/mail';
-
-export default async function handler(req, res) {
+import mailer from 'nodemailer';
+export default async function sendEmail(req, res) {
+    const smtpTransport = mailer.createTransport({
+        host: 'smtp.yandex.ru',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'liesellib@yandex.ru',
+            pass: '08032002LLL'
+        },
+        tls: {rejectUnauthorized: false},
+    }, {
+        from: 'krylevsky-test <liesellib@yandex.ru>'
+    });
     const message = {
         to: 'liesellib@yandex.ru',
         subject: `Письмо с сайта krylevsky-test от ${req.body.name}`,
@@ -11,7 +22,18 @@ export default async function handler(req, res) {
         	Сообщение: ${req.body.message},
         `,
     };
-    await sendEmail(message);
+    await new Promise((resolve, reject) => {
+        smtpTransport.sendMail(message, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email sent successfully", info);
+            }
+            smtpTransport.close();
+        });
+    });
+
     console.log(`BEFORE RESPONSE ${JSON.stringify(res)}`);
     res.send(`Спасибо за заявку, ${req.body.name}!`);
 }
+
