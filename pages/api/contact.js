52990cbe90f58b9591d5df543d7c4af12450f8b6
lisa <elizabet.libet@gmail.com>
2021-11-17 06:53:25 +0500

    edit form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,17 +1,4 @@
 import sendEmail from './../../lib/mail';
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin",'https://legal-aid-liard.vercel.app/');
-    res.setHeader("Access-Control-Allow-Credentials","true");
-    res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-xsrf-token, X-Requested-With, Accept, Expires, Last-Modified, Cache-Control");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
-    next();
-});
-
 
 export default async function handler(req, res) {
     const message = {
